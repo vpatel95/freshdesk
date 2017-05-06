0201d73b6034acb3cbf4a4cc33e50c737f0d03f4
Ved Patel <vedmpatel@gmail.com>
2017-05-06 17:07:17 +0530

    hospital emergency final

--- app/Http/Controllers/App/APIController.php
+++ app/Http/Controllers/App/APIController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\UserDetail;
 use App\Hospital;
 use App\Ambulance;
+use App\PoliceStation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Collection;
